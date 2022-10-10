039b205007075e25486b4513948d80067086a0f9
Utkir <utkirkurbanov2700@gmail.com>
2022-10-10 17:25:53 +0500

    1-st commit

---
+++ code.js
@@ -0,0 +1,27 @@
+var divs = document.querySelectorAll("div");
+var link = document.querySelector("a");
+
+for (var i = 0; i < divs.length; i++) {
+  divs[i].addEventListener("click", function (event) {
+    event.stopPropagation();
+    console.log(this.getAttribute("id"));
+  });
+}
+
+link.addEventListener("click", handleLinkClick);
+
+function handleLinkClick(event) {
+  event.preventDefault();
+
+  var div = divs[0];
+
+  //   if (div.style.display === "none") {
+  //     div.style.display = "flex";
+  //   } else {
+  //     div.style.display = "none";
+  //   }
+
+  div.style.display = div.style.display === "none";
+
+  console.log(div.style.display);
+}
